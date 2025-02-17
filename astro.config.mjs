e8d5d09e47396dd53a94329fd96624566731088d
Tjorn <74906283+Nrojt@users.noreply.github.com>
2025-02-17 14:13:40 +0100

    Add environment variables, update dependencies, and set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+
+import { loadEnv } from "vite";
+
+const { SITE_URL, BASE_PATH } = loadEnv(
+  process.env.NODE_ENV || "",
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'My Docs',
-        social: {
-            github: 'https://github.com/withastro/starlight',
+  integrations: [
+    starlight({
+      title: "Fishbones Studio",
+      social: {
+        github: "https://github.com/Fishbones-Studio",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
         },
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-        }), sitemap(), mdx()],
+        {
+          label: "Game Ideas",
+          autogenerate: { directory: "ideas" },
+        },
+      ],
+    }),
+    sitemap(),
+    mdx(),
+  ],
+  site: SITE_URL || "http://localhost:4321/",
+  base: BASE_PATH,
 });
---
+++ src/content/docs/ideas/chicken_fight_ring.md
@@ -0,0 +1,4 @@
+---
+title: Chicken Fight Ring (TBC)
+description: 3D low poly, Round-based, low poly art style, customizable playable characters, PC
+---

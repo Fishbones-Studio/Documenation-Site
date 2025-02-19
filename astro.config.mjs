e8253e060bef67e8753d321c1b5dcc71cbf6ac3a
CenkerA08 <cenker08@live.nl>
2025-02-19 16:10:58 +0100

    renamed file for a better url

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,8 +36,8 @@ export default defineConfig({
         },
         {
           label: "Prototype",
-          autogenerate: { directory: "prototype"}
-        },
+          autogenerate: { directory: "prototype"},
+        },        
       ],
     }),
     sitemap(),
